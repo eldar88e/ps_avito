71fb272d8cbb635364ac804e4723e4cc72bb1c85
Eldar Eminov <em88el@gmail.com>
2024-02-23 12:51:20 +0300

    Update cron

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # Cron jobs
   config.good_job.cron = {
     storage_maintenance: {
-      cron: "0 10 * * *",
+      cron: "0 10,18 * * *",
       class: "TopGamesJob",
       set: { priority: 10 }, # additional ActiveJob properties; can also be a lambda/proc e.g. `-> { { priority: [1,2].sample } }`
       description: "Populate the Google Sheet for the Avito." # optional description that appears in Dashboard
