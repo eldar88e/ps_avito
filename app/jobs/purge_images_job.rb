44a95659ef9d561168ff8f4fc8e72e1cdceaf5c3
Eldar Eminov <em88el@gmail.com>
2024-03-01 16:17:57 +0300

    Update PurgeImagesJob

--- app/jobs/purge_images_job.rb
+++ app/jobs/purge_images_job.rb
@@ -2,11 +2,10 @@ class PurgeImagesJob < ApplicationJob
   queue_as :default
 
   def perform(**args)
-    site  = args[:site]
     games = Game.order(:top)
     games.each do |game|
-      if game.images.attached? && game.images.blobs.any? { |i| i.metadata[:site] == site }
-        game.images.each { |image| image.purge if image.blob.metadata[:site] == site }
+      if game.images.attached? && (args[:all] || game.images.blobs.any? { |i| i.metadata[:site] == args[:site] })
+        game.images.each { |image| image.purge if args[:all] || image.blob.metadata[:site] == args[:site] }
       end
     end
 
