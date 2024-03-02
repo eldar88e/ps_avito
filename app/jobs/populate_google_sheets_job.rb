43bdc002b22d7fce5de28747f8d22e7b3f432d62
Eldar Eminov <em88el@gmail.com>
2024-03-02 10:07:21 +0300

    Add the make_name to populate_google_sheets_job

--- app/jobs/populate_google_sheets_job.rb
+++ app/jobs/populate_google_sheets_job.rb
@@ -14,7 +14,7 @@ class PopulateGoogleSheetsJob < ApplicationJob
     games.each_slice(100) do |games_slice |
       games_slice.each do |game|
         worksheet[idx, 7]  = game.sony_id
-        worksheet[idx, 12] = game.name
+        worksheet[idx, 12] = make_name(game)
         worksheet[idx, 13] = description(game, args[:site])
         worksheet[idx, 14] = game.price
         worksheet[idx, 15] = make_image(game, args[:site])
@@ -29,6 +29,34 @@ class PopulateGoogleSheetsJob < ApplicationJob
   end
 
   private
+
+  def make_name(game)
+    raw_name = game.name
+    platform  = game.platform
+
+    if platform == 'PS5, PS4' || platform.match?(/PS4/) #ps4, ps5
+      prefix = '(PS5 and PS4)'
+      if raw_name.downcase.match?(/ps4/)
+        if raw_name.downcase.match?(/ps5/)
+          raw_name
+        else
+          raw_name.sub('(PS4)', '').sub('PS4', '') + prefix
+        end
+      else
+        if raw_name.downcase.match?(/ps5/)
+          raw_name.sub('(PS5)', '').sub('PS5', '') + prefix
+        else
+          raw_name + prefix
+        end
+      end
+    else #ps5
+      if raw_name.downcase.match?(/ps5/)
+        raw_name
+      else
+        raw_name + '(PS5)'
+      end
+    end
+  end
 
   def make_image(game, site)
     if game.images.attached? && game.images.blobs.any? { |i| i.metadata[:site] == site }
