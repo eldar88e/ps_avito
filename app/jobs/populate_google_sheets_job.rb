ee8905e9338ff83b01c0b32a9e160bbd3192585e
Eldar Eminov <em88el@gmail.com>
2024-03-02 16:51:36 +0300

    Fix make_name method

--- app/jobs/populate_google_sheets_job.rb
+++ app/jobs/populate_google_sheets_job.rb
@@ -1,8 +1,5 @@
-require_relative './pupulate_help'
-
 class PopulateGoogleSheetsJob < ApplicationJob
   include Rails.application.routes.url_helpers
-  include PopulateHelp
   queue_as :default
 
   def perform(**args)
@@ -29,5 +26,49 @@ class PopulateGoogleSheetsJob < ApplicationJob
   rescue => e
     TelegramService.new("Error #{self.class} || #{e.message}").report
     raise
+  end
+
+  private
+
+  def make_name(game)
+    raw_name = game.name
+    platform  = game.platform
+
+    if platform == 'PS5, PS4' || platform.match?(/PS4/) #ps4, ps5
+      prefix = '(PS5 and PS4)'
+      if raw_name.downcase.match?(/ps4/)
+        if raw_name.downcase.match?(/ps5/)
+          raw_name
+        else
+          raw_name.sub('(PS4)', '').sub('PS4', '') + prefix
+        end
+      else
+        if raw_name.downcase.match?(/ps5/)
+          raw_name.sub('(PS5)', '').sub('PS5', '') + prefix
+        else
+          raw_name + prefix
+        end
+      end
+    else #ps5
+      if raw_name.downcase.match?(/ps5/)
+        raw_name
+      else
+        raw_name + '(PS5)'
+      end
+    end
+  end
+
+  def make_image(game, site)
+    if game.images.attached? && game.images.blobs.any? { |i| i.metadata[:site] == site }
+      image = game.images.find { |i| i.blob.metadata[:site] == site }
+      rails_blob_url(image, host: 'server.open-ps.ru')
+    else
+      nil
+    end
+  end
+
+  def description(game, site)
+    method_name = "desc_#{site}".to_sym
+    DescriptionService.new(game).send(method_name)
   end
 end
