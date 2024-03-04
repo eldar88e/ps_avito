d2d8b94883c2fd9f367b8187ee88315b1b213b4c
Eldar Eminov <em88el@gmail.com>
2024-03-05 01:01:29 +0300

    Add Stores controller

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,58 @@
+class StoresController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @stores = Store.all
+  end
+
+  def show
+    @store = Store.find(params[:id])
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+
+    if @store.save
+      redirect_to @store, notice: 'Store was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @store = Store.find(params[:id])
+  end
+
+  def update
+    @store = Store.find(params[:id])
+    store_params[:description].squeeze!(' ')
+    store_params[:description].chomp!
+    if @store.update(store_params)
+      redirect_to @store, notice: 'Store was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @store = Store.find(params[:id])
+    notice =
+      if @store&.destroy
+        'Store was successfully deleted.'
+      else
+        'Store was not deleted!'
+      end
+    redirect_to stores_path, notice: notice
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:manager_name, :var, :ad_status, :category, :goods_type, :ad_type, :address,
+                                  :description, :condition, :allow_email, :contact_phone, :watermark_params, :table_id)
+  end
+end
