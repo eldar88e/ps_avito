17f7fb75ae67a62d7a64d18d074d1483b836db24
Eldar Eminov <em88el@gmail.com>
2024-03-11 19:59:26 +0300

    Update main_populate_job

--- app/jobs/main_populate_job.rb
+++ app/jobs/main_populate_job.rb
@@ -4,7 +4,6 @@ class MainPopulateJob < ApplicationJob
   def perform
     new_games = TopGamesJob.perform_now
     GameImageDownloaderJob.perform_now if new_games > 0
-    TelegramService.new("✅ Downloaded #{new_games} new game images.").report
     WatermarksSheetsJob.perform_now
   end
 end
