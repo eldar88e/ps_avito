85ee5cc5d4f5c2f9919907b072ae5184ef566aa7
Eldar Eminov <em88el@gmail.com>
2024-03-21 07:00:09 +0300

    Update the add_text method

--- app/services/watermark_service.rb
+++ app/services/watermark_service.rb
@@ -76,7 +76,7 @@ class WatermarkService
 
     params               = layer[:params].is_a?(Hash) ? layer[:params] : eval(layer[:params]).transform_keys { |key| key.to_s }
     text_obj             = Magick::Draw.new
-    text_obj.font        = layer[:img] || params['font'] if layer[:img] || params['font']
+    text_obj.font        = layer[:img] || params['font'] if layer[:img].present? || params['font'].present?
     text_obj.pointsize   = params['pointsize'] || 42  # Размер шрифта
     text_obj.fill        = params['fill'] || 'white'
     text_obj.stroke      = params['stroke'] || 'white'   # Цвет обводки текста
