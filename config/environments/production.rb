a0e168aca2ec72f521be6ddafac6360fbc479d4b
Eldar Eminov <em88el@gmail.com>
2024-07-19 11:40:10 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable' # nil
-  config.action_cable.url = "ws://server.open-ps.ru/cable"
-  config.action_cable.allowed_request_origins = [ "http://server.open-ps.ru", /http:\/\/server.open-ps.*/ ]
+  # config.action_cable.url = "ws://server.open-ps.ru/cable"
+  # config.action_cable.allowed_request_origins = [ "http://server.open-ps.ru", /http:\/\/server.open-ps.*/ ]
+  config.action_cable.disable_request_forgery_protection = true
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
