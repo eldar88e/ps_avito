954cab8f82175ea3d7df76fa90d7abde1fbd0dd9
Eldar Eminov <em88el@gmail.com>
2024-08-03 19:43:12 +0300

    Update CheckAvitoErrorsJob

--- app/jobs/check_avito_errors_job.rb
+++ app/jobs/check_avito_errors_job.rb
@@ -12,15 +12,15 @@ class CheckAvitoErrorsJob < ApplicationJob
       end
 
     stores.each do |store|
-      count_ban = 0
-      avito     = AvitoService.new(store: store)
+      avito = AvitoService.new(store: store)
       next if avito.token_status == 403
 
       url         = 'https://api.avito.ru/autoload/v2/reports/last_completed_report'
       last_report = fetch_and_parse(avito, url)
       next unless last_report
 
-      report_url = "https://api.avito.ru/autoload/v2/reports/#{last_report['report_id']}"
+      report_id  = last_report['report_id']
+      report_url = "https://api.avito.ru/autoload/v2/reports/#{report_id}"
       report     = fetch_and_parse(avito, report_url)
       next unless report
 
@@ -31,28 +31,28 @@ class CheckAvitoErrorsJob < ApplicationJob
 
       next unless error_sections['sections'].find { |i| i['slug'] == 'error_blocked' }
 
-      url         = "#{report_url}/items?sections=error_blocked"
-      blocked     = fetch_and_parse(avito, url)['items']
-      blocked_ids = blocked.map { |i| i['ad_id'] }
-
-      blocked_ids.each do |id|
-        store.ban_lists.create(ad_id: id, expires_at: Time.current + 1.month, report_id: report_id)
-        count_ban += 1
-      rescue ActiveRecord::RecordNotUnique
-        old_ad = store.ban_lists.find_by(ad_id: id)
-        if old_ad.expires_at > Time.current
-          next
-        else
-          old_ad.update(expires_at: Time.current)
-          count_ban += 1
-        end
-      end
-
-      msg = "✅ Added #{count_ban} bans for #{store.manager_name}"
-      TelegramService.call(current_user, msg) if count_ban > 0
+      url     = "#{report_url}/items?sections=error_blocked"
+      blocked = fetch_and_parse(avito, url)
+      add_ban_ad(store, blocked, report_id) if blocked
     end
 
     nil
+  end
+
+  def add_ban_ad(store, blocked, report_id)
+    count_ban = 0
+    blocked['items'].each do |item|
+      id             = item['ad_id']
+      ban_list_entry = store.ban_lists.find_or_initialize_by(ad_id: id)
+
+      if ban_list_entry.new_record? || ban_list_entry.expires_at <= Time.current
+        ban_list_entry.update(expires_at: Time.current + 1.month, report_id: report_id)
+        count_ban += 1
+      end
+    end
+
+    msg = "✅ Added #{count_ban} bans for #{store.manager_name}"
+    TelegramService.call(store.user, msg) if count_ban > 0
   end
 
   def send_error_sections(section, user, account)
