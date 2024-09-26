4e5b5904233cbcbc06689d748f4b677545260604
Eldar Eminov <em88el@gmail.com>
2024-09-26 16:32:41 +0300

    Update check_status_job and add avito_id column to ads

--- app/jobs/avito/check_status_job.rb
+++ app/jobs/avito/check_status_job.rb
@@ -1,5 +1,6 @@
 class Avito::CheckStatusJob < ApplicationJob
   queue_as :default
+  PER_PAGE = 50
 
   def perform(**args)
     stores =
@@ -10,26 +11,60 @@ class Avito::CheckStatusJob < ApplicationJob
 
         current_user(args[:user_id]).stores.where(active: true)
       end
-    current_user = stores.first.user
+    user = stores.first.user
 
     stores.each do |store|
+      low_rating = []
       avito = AvitoService.new(store: store)
       next if avito.token_status == 403
 
-      page = 0
+      url      = 'https://api.avito.ru/core/v1/accounts/self'
+      response = avito.connect_to(url)
+      next if response.status != 200
+
+      account_id  = JSON.parse(response.body)['id']
+      ads_db      = store.ads.load
+      page        = 0
+      ads_cache   = {}
+      items_cache = {}
       loop do
-        url = "https://api.avito.ru/core/v1/items?#{page}"
-        ads = fetch_and_parse(avito, url)
+        url = "https://api.avito.ru/core/v1/items?page=#{page}&per_page=#{PER_PAGE}"
+        ads_cache[:"#{page}"] ||= fetch_and_parse(avito, url)
+        ads = ads_cache[:"#{page}"]
         break if ads.nil? || ads["resources"].blank?
-        ids        = ads["resources"].map { |i| i['id'] }
-        prev_date  = Time.current.prev_month.to_date.to_s
-        payload    = {'dateFrom': prev_date, 'itemIds': ids, 'periodGrouping': "month"}
-        account_id = '' # TODO получить id account
-        url        = "https://api.avito.ru/stats/v1/accounts/#{account_id}/items"
-        response   = avito.connect_to(url, :post, payload)
-        binding.pry
+
+        ids       = ads["resources"].map { |i| i['id'] }
+        date_from = Time.current.beginning_of_month.to_date.to_s # TODO добавить .prev_month
+        date_to   = Time.current.beginning_of_month.to_date.to_s
+        payload   = { 'dateFrom': date_from, 'itemIds': ids, 'periodGrouping': 'month' } # 'dateTo': date_to,
+        url       = "https://api.avito.ru/stats/v1/accounts/#{account_id}/items"
+        items_cache[:"#{page}"] ||= fetch_and_parse(avito, url, :post, payload)
+        items_raw = items_cache[:"#{page}"]
+        items     = items_raw['result']['items']
+        items.each do |item|
+          next if item['stats'].present?
+
+          avito_id = item['itemId']
+          options  = { avito_id: avito_id }
+          updated  = update_ad(ads_db, **options)
+          next if updated
+
+          url      = "https://api.avito.ru/autoload/v2/items/ad_ids?query=#{avito_id}"
+          response = fetch_and_parse(avito, url)
+          next if response.nil?
+
+          ad_id = response['items'][0]['ad_id']
+          low_rating << ad_id
+          options[:id] = ad_id
+          update_ad(ads_db, **options)
+          sleep rand(0.7..1.5)
+        end
         page += 1
+      rescue => e
+        TelegramService.call(user, e.message)
       end
+      msg = "✅ Store #{store} have #{low_rating.size} low rating ads.\n#{low_rating.join(", ")}"
+      TelegramService.call(user, msg) if low_rating.size > 0
     end
 
     nil
@@ -37,12 +72,24 @@ class Avito::CheckStatusJob < ApplicationJob
 
   private
 
+  def update_ad(ads_db, **args)
+    avito_id = args[:id] ? args.delete(:avito_id) : nil
+    ad       = ads_db.find { |i| i[args.keys.first] == args.values.first }
+    puts avito_id # TODO убрать
+    if ad.present?
+      options = {}
+      # options[:deleted]  = 1 # TODO раскоментировать и проверить
+      options[:avito_id] = avito_id if ad.avito_id.nil?
+      ad.update(options)
+    end
+  end
+
   def send_error_sections(section, user, account)
     TelegramService.call(user, "‼️#{section['title']} #{section['count']} на аккаунте #{account}.")
   end
 
-  def fetch_and_parse(avito, url)
-    response = avito.connect_to(url)
+  def fetch_and_parse(avito, url, method = :get, payload=nil)
+    response = avito.connect_to(url, method, payload)
     return if response.nil? || response.status != 200
 
     JSON.parse(response.body)
