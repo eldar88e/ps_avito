46665250a3350a45d5badf0e2a87467997a7228d
Eldar Eminov <em88el@gmail.com>
2024-10-16 21:57:56 +0300

    Update Action Cable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = '/cable' # nil
-  config.action_cable.url = "wss://server.open-ps.ru/cable"
+  config.action_cable.url = "ws://server.open-ps.ru/cable"
   config.action_cable.allowed_request_origins = %w[http://server.open-ps.ru http://server.open-ps.ru]
   # config.action_cable.disable_request_forgery_protection = true
 
