09dc9056e73b00facf719a677f6ed5895572ac5c
Eldar Eminov <em88el@gmail.com>
2024-10-20 18:47:26 +0300

    Update webhooks_controller

--- app/controllers/avito/webhooks_controller.rb
+++ app/controllers/avito/webhooks_controller.rb
@@ -1,22 +1,24 @@
-class Avito::WebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token
+module Avito
+  class WebhooksController < ApplicationController
+    skip_before_action :verify_authenticity_token
 
-  def receive
-    #data = request.body.read
-    TelegramService.call(User.first, 'work')
+    def receive
+      data = request.body.read
+      TelegramService.call(User.first, data)
 
-    # webhook_event = JSON.parse(data)
-    # Обработайте событие
-    # case webhook_event['event_type']
-    # when 'order_created'
-    #   # Логика для события создания заказа
-    # when 'order_updated'
-    #   # Логика для события обновления заказа
-    # else
-    #   # Логика для других событий
-    # end
-    render json: { status: 'ok' }, status: :ok
-  rescue => e
-    TelegramService.call(User.first, e.message)
+      # webhook_event = JSON.parse(data)
+      # Обработайте событие
+      # case webhook_event['event_type']
+      # when 'order_created'
+      #   # Логика для события создания заказа
+      # when 'order_updated'
+      #   # Логика для события обновления заказа
+      # else
+      #   # Логика для других событий
+      # end
+      render json: { status: 'ok' }, status: :ok
+    rescue => e
+      TelegramService.call(User.first, e.message)
+    end
   end
 end
