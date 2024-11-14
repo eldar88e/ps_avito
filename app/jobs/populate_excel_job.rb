41a9733d797ba87a99e1ba292617883f8b74fd7b
Eldar Eminov <em88el@gmail.com>
2024-11-14 05:18:57 +0300

    Add GamePriceService

--- app/jobs/populate_excel_job.rb
+++ app/jobs/populate_excel_job.rb
@@ -28,10 +28,11 @@ class PopulateExcelJob < ApplicationJob
         # msg = "Skipped because there is no image for #{file_id}"
         # TelegramService.call(user, msg)
 
+        price = GamePriceService.call(game.price_tl, store)
         worksheet.append_row(
           [ad.id, ad.avito_id, current_time, store.ad_status, store.category, store.goods_type, store.ad_type,
-           store.type, make_platform(game), make_local(game), ad.full_address || address.store_address, make_title(game),
-           make_description(game, store, address), store.condition, make_price(game.price_tl, store), store.allow_email,
+           store.type, make_platform(game), make_local(game), ad.full_address || address.store_address,
+           make_title(game), make_description(game, store, address), store.condition, price, store.allow_email,
            store.manager_name, store.contact_phone, store.contact_method, img_url]
         )
       end
@@ -144,31 +145,6 @@ class PopulateExcelJob < ApplicationJob
       desc_product = store.description + store.desc_product.to_s
       desc_product.gsub('[name]', model.title).gsub('[addr_desc]', address.description || '')
                   .gsub('[manager]', store.manager_name).gsub('[desc_product]', model.description).squeeze(' ').strip
-    end
-  end
-
-  def make_price(price, store)
-    percent       = store.percent || 0
-    exchange_rate = make_exchange_rate(price)
-    price_rub     = price * exchange_rate
-    round_up_price(price_rub + (price_rub * percent / 100.0))
-  end
-
-  def round_up_price(price)
-    (price / 10.to_f).round * 10
-  end
-
-  def make_exchange_rate(price)
-    if price < 300
-      ENV.fetch("LOWEST_PRICE") { 5.5 }.to_f
-    elsif price >= 300 && price < 800
-      ENV.fetch("LOW_PRICE") { 5 }.to_f
-    elsif price >= 800 && price < 1200
-      ENV.fetch("MEDIAN_PRICE") { 4.5 }.to_f
-    elsif price >= 1200 && price < 1700
-      ENV.fetch("HIGH_PRICE") { 4.3 }.to_f
-    else
-      ENV.fetch("HIGHEST_PRICE") { 4 }.to_f
     end
   end
 end
---
+++ app/services/game_price_service.rb
@@ -0,0 +1,30 @@
+class GamePriceService
+  class << self
+    def call(price, store)
+      percent       = store.percent || 0
+      exchange_rate = make_exchange_rate(price)
+      price_rub     = price * exchange_rate
+      round_up_price(price_rub + (price_rub * percent / 100.0))
+    end
+
+    private
+
+    def round_up_price(price)
+      (price / 10.to_f).round * 10
+    end
+
+    def make_exchange_rate(price)
+      if price < 300
+        ENV.fetch("LOWEST_PRICE") { 5.5 }.to_f
+      elsif price >= 300 && price < 800
+        ENV.fetch("LOW_PRICE") { 5 }.to_f
+      elsif price >= 800 && price < 1200
+        ENV.fetch("MEDIAN_PRICE") { 4.5 }.to_f
+      elsif price >= 1200 && price < 1700
+        ENV.fetch("HIGH_PRICE") { 4.3 }.to_f
+      else
+        ENV.fetch("HIGHEST_PRICE") { 4 }.to_f
+      end
+    end
+  end
+end
