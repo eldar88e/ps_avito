d607ae09fbac19b6b6fa6188e16ded66c7a96c8b
Eldar Eminov <em88el@gmail.com>
2024-11-14 17:43:14 +0300

    Add UpdatePriceJob to MainPopulateJob

--- app/jobs/main_populate_job.rb
+++ app/jobs/main_populate_job.rb
@@ -6,6 +6,7 @@ class MainPopulateJob < ApplicationJob
     settings     = current_user.settings.pluck(:var, :value).to_h
     new_games    = TopGamesJob.perform_now(settings: settings, user: current_user)
     GameImageDownloaderJob.perform_now(settings: settings, user: current_user) if new_games > 0
-    WatermarksSheetsJob.perform_now(user: current_user)
+    WatermarksSheetsJob.perform_later(user: current_user)
+    Avito::UpdatePriceJob.perform_later
   end
 end
--- app/jobs/save_image_job.rb
+++ app/jobs/save_image_job.rb
@@ -22,9 +22,6 @@ class SaveImageJob < ApplicationJob
   rescue StandardError => e
     msg = "Аккаунт: #{store.manager_name}\nID: #{product.send(id)}\nError: #{e.message}"
     msg << "\nТовар: #{product.is_a?(Game) ? product.name : product.title}"
-    if file_id.match?(/[A-Z]/)
-      # msg << "\nhttps://store.playstation.com/store/api/chihiro/00_09_000/container/TR/tr/99/#{product.send(id)}/0/image"
-    end
     TelegramService.call(user, msg)
   end
 
