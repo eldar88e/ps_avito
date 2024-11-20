fc29aed333b5b83681544b61f9550635984a7bb1
Eldar Eminov <em88el@gmail.com>
2024-11-20 07:07:30 +0300

    Refactor

--- app/services/game_title_service.rb
+++ app/services/game_title_service.rb
@@ -10,16 +10,22 @@ class GameTitleService
   end
 
   def self.two_platforms(raw_name)
-    prefix = ' PS5 и PS4'
-    if raw_name.downcase.include?('ps4')
-      return raw_name if raw_name.downcase.include?('ps5')
+    return raw_name if contains_both_platforms?(raw_name)
 
-      raw_name.sub('(PS4)', '').sub('PS4', '').strip[0..39] + prefix
-    elsif raw_name.downcase.include?('ps5')
-      raw_name.sub('(PS5)', '').sub('PS5', '').strip[0..39] + prefix
-    else
-      raw_name.strip[0..39] + prefix
-    end
+    prefix = ' PS5 и PS4'
+    "#{clean_name(raw_name)}#{prefix}"
+  end
+
+  def self.contains_both_platforms?(raw_name)
+    raw_name.downcase.include?('ps4') && raw_name.downcase.include?('ps5')
+  end
+
+  def self.clean_name(raw_name)
+    raw_name.sub('(PS4)', '')
+            .sub('(PS5)', '')
+            .sub('PS4', '')
+            .sub('PS5', '')
+            .strip[0..39]
   end
 
   def self.ps5_platform(raw_name)
