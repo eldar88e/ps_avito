a2b6fa10d0bf32087815108bb2fd4dccea12a61d
Eldar Eminov <em88el@gmail.com>
2024-11-26 00:51:51 +0300

    Fix datepicker for ad

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import { Datepicker } from 'stimulus-datepicker'
 
 const application = Application.start()
+application.register('datepicker', Datepicker)
 
 // Configure Stimulus development experience
 application.debug = false
